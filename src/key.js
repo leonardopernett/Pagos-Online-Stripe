5fbe73ba0352fe7277f60442a5ffc8d4901044dd
leonardo pernett <pernettleonardo@gmail.com>
2020-07-03 19:46:17 -0500

    complete pay

---
+++ src/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PUBLIC_KEY:"pk_test_kvZRSrXGYRq7LqAEcCPkKal700PWz1vLeC",
+    SECRET_KEY:"sk_test_0GtckB81c7iRloQRaNDBJ2v900T93U1Jex"
+
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,26 @@
-const {Router}= require('express')
-const stripe = require('stripe')('sk_test_0GtckB81c7iRloQRaNDBJ2v900T93U1Jex')
-const router = Router()
+const { Router } = require("express");
+const { SECRET_KEY, PUBLIC_KEY } = require("../key");
+const stripe = require("stripe")(SECRET_KEY);
+const router = Router();
 
-router.get('/',(req,res)=>{
-  
-     res.render('index')
-  
-})
+router.get("/", (req, res) => {
+  res.render("index", { key: PUBLIC_KEY });
+});
 
-
-router.post('/pay',async (req,res)=>{
-   try {
-    console.log(req.body)
-
-    // recibe los datos 
-    const customer = await stripe.customers.create({
-        email:req.body.stripeEmail,
-        source:req.body.stripeToken
-    })
-    //almacena orden compr 
-    const charge = await stripe.charges.create({
-        amount: 3000,
-        currency: 'usd', 
-        source: customer.id,
-        description: 'software video',
-      })
-       console.log(charge.id)
-        res.render('download')
-   } catch (error) {
-      console.log(error)
-   }
+router.post("/pay", async (req, res) => {
+  const customer = await stripe.customers.create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
   });
-module.exports=router;
+
+  const charge = await stripe.charges.create({
+      amount: 3000, // Charing Rs 25
+      description: "Web Development Product",
+      currency: "usd",
+      customer: customer.id,
+  });
+
+  console.log(charge.id);
+  res.render("download"); 
+});
+module.exports = router;
